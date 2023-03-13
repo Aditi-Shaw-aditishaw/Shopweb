06eaa4688dc8fe3873c518fcd78aaec6a44f6dbf
Aditi-Shaw-aditishaw <127743490+Aditi-Shaw-aditishaw@users.noreply.github.com>
2023-03-13 18:53:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -33,3 +33,43 @@ function getAge(dob) {
 
     return age;
 }
+
+function validateForm() {
+    let dobInput = document.getElementById("dob");
+    let dob = new Date(dobInput.value);
+    let today = new Date();
+    let age = today.getFullYear() - dob.getFullYear();
+    let m = today.getMonth() - dob.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < dob.getDate())) {
+        age--;
+    }
+
+    if (age < 18 || age > 55) {
+        let errorMessage = document.getElementById("error-message");
+        errorMessage.textContent = "You must be between 18 and 55 years old.";
+        errorMessage.style.display = "block";
+        return false;
+    }
+
+    let entries = document.getElementById("entries");
+    entries.innerHTML += "<p>" + dobInput.value + "</p>";
+}
+
+function addEntry() {
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var dob = document.getElementById("dob").value;
+    var password = document.getElementById("password").value;
+
+    // Create a new entry element
+    var entry = document.createElement("div");
+    entry.innerHTML = "<b>Name:</b> " + name + "<br>" +
+        "<b>Email:</b> " + email + "<br>" +
+        "<b>dob</b> " + dob + "<br>";
+    "<b>password:</b> " + password + "<br>";
+
+    // Append the new entry to the entries box
+    var entriesBox = document.getElementById("entries-box");
+    entriesBox.appendChild(entry);
+}

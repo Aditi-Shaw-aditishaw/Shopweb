88df72d27295235e1ba73989ac5f354024c083e2
Aditi-Shaw-aditishaw <127743490+Aditi-Shaw-aditishaw@users.noreply.github.com>
2023-03-13 19:36:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,66 @@
-const form = document.getElementById("registration-form");
-const entriesList = document.getElementById("entries-list");
+let userForm = document.getElementById("user-form");
+
+const retrieveEntries = () => {
+    let entries = localStorage.getItem("user-entries");
+
+    if (entries) {
+
+        entries = JSON.parse(entries);
+    } else { entries = []; }
+
+    return entries;
+}
+
+let userEntries = retrieveEntries(;)
+
+const displayEntries = () => {
+
+    const entries = retrieveEntries();
+    entries.map(entry) {};
+}
+
+const nameCell '<td class="border px-4 py-2">$(entry.namn)</td>';
+const emailCell '<td class="border px-4 py-2">$(entry.email)</td>';
+const passwordCell '<td class "border px-4 py-2">$(entry.password)</td>';
+
+const dobcell '<td class" border px-4 py-2">$(entry.dob)</td>'
+const acceptTermscell '<td> class="border px-4 py-2">$(entry.acceptedTerms)</td>;'
+
+const row = < tr > { nameCell }
+$ { emailcell }
+$ { passwordcell }
+$ { dobCell }
+$ { acceptTernstell } < /tr> |
+return row;
+.join("\n")
+
+const table = < table class = "table-auto w-full" > < tr >
+
+    <
+    th class = "px-4 py-2" > name < /th>
+
+<
+th class = "px- py-2" > Email < /t>
+
+<
+th class = "px- py-2" > Password < /th> <
+    th class = "px- py-2" > dob - < /th>
+
+<
+th class = "px-4 py-2" > accepted terms ? < /th>
+
+<
+/tr>s{tableEntries} </table >
+<
+th class = "px-4 py-2" > accepted terms ? < /th> 
+
+<
+/tr>s(tableEntries) </table >
+
+
+let details = document.getElementById("user-entries!");
+
+details.innerHTML = table;
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
